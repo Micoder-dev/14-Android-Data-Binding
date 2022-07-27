cc83a69ea22098c486ace2db90f1d5593fcb1871
Micoder-dev <micoder.com@gmail.com>
2022-07-27 22:23:19 +0530

    Handling Click Events

--- app/src/main/java/com/example/androiddatabinding/MainActivity.java
+++ app/src/main/java/com/example/androiddatabinding/MainActivity.java
@@ -3,17 +3,24 @@ package com.example.androiddatabinding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.androiddatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ClickEventHandler mClickEventHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mClickEventHandler = new ClickEventHandler(this);
 
         activityMainBinding.setMyviewmodel(new MyViewModel());
 
@@ -23,7 +30,28 @@ public class MainActivity extends AppCompatActivity {
 
         activityMainBinding.txtTaskColor.setText("RED");
 
-        activityMainBinding.setLifecycleOwner(this);
+        //activityMainBinding.setLifecycleOwner(this);
+
+        activityMainBinding.setClickEventHandler(mClickEventHandler);
 
     }
+
+    public class ClickEventHandler {
+
+        private Context mContext;
+
+        public ClickEventHandler(Context context) {
+            mContext = context;
+        }
+
+        public void textViewClick(View view) {
+            Toast.makeText(mContext, "TextView clicked", Toast.LENGTH_SHORT).show();
+        }
+
+        public void textViewClickWithParams(View view, String category) {
+            Toast.makeText(mContext, category, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
