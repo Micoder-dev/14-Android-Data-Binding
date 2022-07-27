22e69bb5d99de24e47a8dde77dc3d466e6a9b493
Micoder-dev <micoder.com@gmail.com>
2022-07-27 21:50:59 +0530

    Implement binding at the activity level

--- app/src/main/java/com/example/androiddatabinding/MainActivity.java
+++ app/src/main/java/com/example/androiddatabinding/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.androiddatabinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.example.androiddatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
     }
 }
